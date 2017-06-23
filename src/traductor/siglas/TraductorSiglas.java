bb4c60489700f4856ab496999ce3376056d567fd
Richardjcs <r.cacuango@gmail.com>
2017-06-23 12:33:44 +0200

    Primer commit

---
+++ src/traductor/siglas/TraductorSiglas.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package traductor.siglas;
+
+/**
+ *
+ * @author richardcj
+ */
+public class TraductorSiglas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.err.println("Hello Friend :)");
+    }
+}
